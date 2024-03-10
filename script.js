b7e2d67851450ac816b8a16419c12c00cd88e64e
Fabiano Terdenge <ft748572@fh-muenster.de>
2024-03-10 11:52:26 +0100

    add alt tag to img

--- script.js
+++ script.js
@@ -7,4 +7,5 @@ function openAnswere(id){
     let answereshown = !answere[id].classList.contains("hide");
     let question = document.getElementsByTagName("img");
     question[id+1].src = answereshown ? "assets/images/icon-minus.svg" : "assets/images/icon-plus.svg";
+    question[id+1].alt = answereshown ? "minus" : "plus";
 }
